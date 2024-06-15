c52f86144f6eeb675cfd3dee8c26019a8beef882
Евгений Аркатов <thejekaarkatov@yandex.ru>
2024-06-15 18:29:44 +0300

    tests routes ready

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_django.asserts import assertRedirects
 
 from http import HTTPStatus
 from django.urls import reverse
@@ -29,10 +30,30 @@ def test_pages_availability_for_anonymous_user(client, name):
 
 
 @pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
     'name',
     ('news:delete', 'news:edit')
 )
-def test_del_and_edit_comment_for_author(author_client, comment, name):
+def test_del_and_edit_comment_for_author(parametrized_client, comment, name, expected_status):
     url = reverse(name, kwargs={'pk': comment.pk})
-    response = author_client.get(url)
-    assert response.status_code == HTTPStatus.OK
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',
+    ('news:delete', 'news:edit')
+)
+def test_del_and_edit_comment_for_anonumous(client, comment, name):
+    login_url = reverse('users:login')
+    url = reverse(name, kwargs={'pk': comment.pk})
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
